586088f7bc6c12fb881df7ae0178f626091c3341
Antonin Royer <antonin.royer84@gmail.com>
2024-03-28 11:03:10 +0100

    backup

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,117 +1,148 @@
 /* ----- NAVIGATION BAR FUNCTION ----- */
-    function myMenuFunction(){
-      var menuBtn = document.getElementById("myNavMenu");
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
 
-      if(menuBtn.className === "nav-menu"){
+    if(menuBtn.className === "nav-menu"){
         menuBtn.className += " responsive";
-      } else {
+    } else {
         menuBtn.className = "nav-menu";
-      }
     }
+}
 
 /* ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING ----- */
-    window.onscroll = function() {headerShadow()};
+window.onscroll = function() {headerShadow()};
 
-    function headerShadow() {
-      const navHeader =document.getElementById("header");
+function headerShadow() {
+    const navHeader =document.getElementById("header");
 
-      if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
-
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
         navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
         navHeader.style.height = "70px";
         navHeader.style.lineHeight = "70px";
-
-      } else {
-
+    } else {
         navHeader.style.boxShadow = "none";
         navHeader.style.height = "90px";
         navHeader.style.lineHeight = "90px";
-
-      }
     }
+}
 
 
 /* ----- TYPING EFFECT ----- */
-   var typingEffect = new Typed(".typedText",{
-      strings : ["DevOps","DSI","Developer"],
-      loop : true,
-      typeSpeed : 100,
-      backSpeed : 80,
-      backDelay : 2000
-   })
+var typingEffect = new Typed(".typedText",{
+    strings : ["DevOps","DSI","Developer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+});
 
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
-   const sr = ScrollReveal({
-          origin: 'top',
-          distance: '80px',
-          duration: 2000,
-          reset: true
-   })
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
 
-  /* -- HOME -- */
-  sr.reveal('.featured-text-card',{})
-  sr.reveal('.featured-name',{delay: 100})
-  sr.reveal('.featured-text-info',{delay: 200})
-  sr.reveal('.featured-text-btn',{delay: 200})
-  sr.reveal('.social_icons',{delay: 200})
-  sr.reveal('.featured-image',{delay: 300})
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
 
 
-  /* -- PROJECT BOX -- */
-  sr.reveal('.project-box',{interval: 200})
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
 
-  /* -- HEADINGS -- */
-  sr.reveal('.top-header',{})
+/* -- HEADINGS -- */
+sr.reveal('.top-header',{})
 
 /* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
 
-  /* -- ABOUT INFO & CONTACT INFO -- */
-  const srLeft = ScrollReveal({
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
     origin: 'left',
     distance: '80px',
     duration: 2000,
     reset: true
-  })
+})
 
-  srLeft.reveal('.about-info',{delay: 100})
-  srLeft.reveal('.contact-info',{delay: 100})
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
 
-  /* -- ABOUT SKILLS & FORM BOX -- */
-  const srRight = ScrollReveal({
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
     origin: 'right',
     distance: '80px',
     duration: 2000,
     reset: true
-  })
+})
 
-  srRight.reveal('.skills-box',{delay: 100})
-  srRight.reveal('.form-control',{delay: 100})
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
 
 
 
 /* ----- CHANGE ACTIVE LINK ----- */
 
-  const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]')
 
-  function scrollActive() {
+function scrollActive() {
     const scrollY = window.scrollY;
 
     sections.forEach(current =>{
-      const sectionHeight = current.offsetHeight,
-          sectionTop = current.offsetTop - 50,
-        sectionId = current.getAttribute('id')
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+            sectionId = current.getAttribute('id')
 
-      if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-
-          document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-
-      }  else {
-
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }  else {
         document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    });
+}
 
-      }
-    })
-  }
+window.addEventListener('scroll', scrollActive);
 
-  window.addEventListener('scroll', scrollActive)
+// Experience Section
+const experience = document.querySelector('#experience');
+const experienceBtn = document.querySelector('#experience-btn');
+// reveal experience section on click
+experienceBtn.addEventListener('click', () => {
+    if (experience.style.display === 'none') {
+        experience.style.display = 'block';
+    }
+    else {
+        experience.style.display = 'none';
+    }
+});
+
+// Project Section
+const projects = document.querySelector('#project');
+const projectsBtn = document.querySelector('#project-btn');
+// reveal project section on click
+projectsBtn.addEventListener('click', () => {
+    if (projects.style.display === 'none') {
+        projects.style.display = 'block';
+    }
+    else {
+        projects.style.display = 'none';
+    }
+});
+
+// Clients Section
+const clients = document.querySelector('#client');
+const clientsBtn = document.querySelector('#client-btn');
+// reveal clients section on click
+clientsBtn.addEventListener('click', () => {
+    if (clients.style.display === 'none') {
+        clients.style.display = 'block';
+    }
+    else {
+        clients.style.display = 'none';
+    }
+});
